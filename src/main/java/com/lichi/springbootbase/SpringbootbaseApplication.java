564c72db2250410ac576488d15c2bbfc0f9dd067
[lychee] <wzyhlj999455@163.com>
2023-01-04 14:25:28 +0800

    修改pom依赖版本管理

--- src/main/java/com/lichi/springbootbase/SpringbootbaseApplication.java
+++ src/main/java/com/lichi/springbootbase/SpringbootbaseApplication.java
@@ -2,10 +2,7 @@ package com.lichi.springbootbase;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.ComponentScans;
 
 /**
  * @SpringBootApplication 来标注一个主程序类，说明这是一个Spring Boot应用
@@ -13,7 +10,6 @@ import org.springframework.context.annotation.ComponentScans;
  */
 @SpringBootApplication
 @EnableCaching
-//@ComponentScan("com.lichi")
 public class SpringbootbaseApplication {
 
     public static void main(String[] args) {
