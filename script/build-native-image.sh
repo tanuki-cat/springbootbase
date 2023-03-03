02517e1f8b06a61ae8a6940eee78b7027736ce6e
[lychee] <wzyhlj999455@163.com>
2023-03-03 14:25:03 +0800

    构建native-image 脚本

---
+++ script/build-native-image.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# shellcheck disable=SC2092
+# shellcheck disable=SC2034
+# shellcheck disable=SC2006
+cd ..
+filename=springbootbase
+suffix=0.0.1-SNAPSHOT.jar
+success=`mvn clean package`
+# shellcheck disable=SC1073
+# shellcheck disable=SC2086
+#if   [ -e "/target/""$filename""$suffix" ]; then
+  # shellcheck disable=SC2164
+cd target
+`native-image -jar "$filename-""$suffix" $filename`
+#fi
+exit 0
