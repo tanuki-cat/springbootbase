db5dc4a133bbd3fd7e966deb0865193e3096a896
[lychee] <wzyhlj999455@163.com>
2023-03-04 12:03:49 +0800

    修改构建脚本

--- script/build-native-image.sh
+++ script/build-native-image.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -v
 # shellcheck disable=SC2092
 # shellcheck disable=SC2034
 # shellcheck disable=SC2006
@@ -8,9 +8,9 @@ suffix=0.0.1-SNAPSHOT.jar
 success=`mvn clean package`
 # shellcheck disable=SC1073
 # shellcheck disable=SC2086
-#if   [ -e "/target/""$filename""$suffix" ]; then
+if   [ -e "./target/""$filename-""$suffix" ]; then
   # shellcheck disable=SC2164
-cd target
-`native-image -jar "$filename-""$suffix" $filename`
-#fi
+  cd target
+  `native-image -jar "$filename-""$suffix" $filename`
+fi
 exit 0
